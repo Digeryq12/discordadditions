2b2658cd95c9f5ca6a3303cf4c60310deb81952c
Digery <cseresz.dani@gmail.com>
2025-10-14 21:27:06 +0200

    Update

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ import os
 import discord
 from dotenv import load_dotenv
 
+bot_prefix = "!"
+embed_color = 431252
+
 load_dotenv()
-token = os.get_env("BOT_TOKEN")
-print(token)
+token = os.getenv("BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+def create_embed(title, content, color, image_url, banner):
+    embed = discord.Embed(title=title, description=content, color=color)
+    if image_url:
+        embed.set_thumbnail(url=image_url)
+    if banner:
+        embed.set_image(url=banner)
+        
+    embed.set_footer(text="DCAdditions by @digeryy")
+    return embed
+
+@client.event
+async def on_message(message):
+    if not message.author == client.user:
+        msg = message.content.lower()
+        if msg == f"{bot_prefix}ping":
+            latency = round(client.latency * 1000)
+            content = f"Pong! {latency}"
+            embed = create_embed("Ping", content, embed_color, None, None)
+            await message.channel.send(embed=embed)
+
+client.run(token)
